8460ab9db27a94c26ac1c58100cc959fcbfd60b8
HenryCodeT <120686@unsaac.edu.pe>
2021-10-21 11:42:56 -0500

    time countdown

--- DOM_Ejercicios_Prácticos/Menú_Hamburguesa/js/index_dom.js
+++ DOM_Ejercicios_Prácticos/Menú_Hamburguesa/js/index_dom.js
@@ -1,3 +1,4 @@
+import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shotcuts } from "./dom/teclado.js";
@@ -7,7 +8,12 @@ const d=document;
 d.addEventListener("DOMContentLoaded", (e) =>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
-    alarm("assets/alarm-clock.mp3", "#activar-alarma","#desactivar-alarma")
+    alarm("assets/alarm-clock.mp3", "#activar-alarma","#desactivar-alarma");
+    countdown(
+        "countdown",
+        "2022-01-13 00:00:00",
+        "Feliz Cumpleaños!!!!!!"
+    );
 })
 
 d.addEventListener("keydown",e=>{
